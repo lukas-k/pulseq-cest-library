34667a986602b0125547787b97524277ec9ea467
mzaiss <moritz.zaiss@uk-erlangen.de>
2021-02-13 12:29:42 +0100

    adjusted so that it looks most in vivo like

--- run_sim_APTw003.m
+++ run_sim_APTw003.m
@@ -12,10 +12,10 @@ m0_offset = seq.definitions('M0_offset');     % m0 offset frequency
 figure(1);
 % M_z = Run_pulseq_cest_Simulation(seq_filename,fullfile(lib_path,'/sim-library/GM_3T_001_bmsim.yaml'));
 % Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset)
-
-M_z = Run_pulseq_cest_Simulation(seq_filename,[lib_path '/sim-library/WM_3T_001_bmsim.yaml']);
-
-Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset)
+% 
+ M_z = Run_pulseq_cest_Simulation(seq_filename,[lib_path '/sim-library/GLIO_3T_001_bmsim.yaml']);
+% 
+ Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset)
 
  M_z = Run_pulseq_cest_Simulation(seq_filename,[lib_path '/sim-library/WM_3T_002_bmsim.yaml']);
 
