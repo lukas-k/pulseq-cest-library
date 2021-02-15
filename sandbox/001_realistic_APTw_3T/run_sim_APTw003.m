3d98ea2e18b4289ddffb769c8fd17599fa0ce87a
mzaiss <moritz.zaiss@uk-erlangen.de>
2021-02-15 22:46:10 +0100

    added some plots and models

--- sandbox/001_realistic_APTw_3T/run_sim_APTw003.m
+++ sandbox/001_realistic_APTw_3T/run_sim_APTw003.m
@@ -9,7 +9,43 @@ offsets_ppm = seq.definitions('offsets_ppm'); % offsets
 m0_offset = seq.definitions('M0_offset');     % m0 offset frequency
 
 %% call standard sim  for WM and GLIO ( with shifted Lorentzian MT)
-figure(2);
+figure('Name','(Super) Lorentz');
+% M_z = Run_pulseq_cest_Simulation(seq_filename,fullfile(lib_path,'/sim-library/GM_3T_001_bmsim.yaml'));
+% Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset)
+% 
+sandbox_path='D:\root\LABLOG\FAU\11_CESTpulseq_standard\pulseq-cest-library\sandbox\001_realistic_APTw_3T';
+ M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/WM_3T_001_bmsim.yaml']);
+ [Z,w]= Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
+
+ M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/WM_3T_003_bmsim.yaml']);
+ [Z1,w]=Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
+ 
+%% call standard sim  for WM and GLIO ( with shifted Lorentzian MT)
+figure('Name','MTC asym');
+% M_z = Run_pulseq_cest_Simulation(seq_filename,fullfile(lib_path,'/sim-library/GM_3T_001_bmsim.yaml'));
+% Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset)
+% 
+sandbox_path='D:\root\LABLOG\FAU\11_CESTpulseq_standard\pulseq-cest-library\sandbox\001_realistic_APTw_3T';
+ M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/WM_3T_003_bmsim.yaml']);
+ [Z,w]= Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
+
+ M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/GLIO_3T_003_bmsim.yaml']);
+ [Z1,w]=Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
+ 
+ %% call standard sim  for WM and GLIO ( with shifted Lorentzian MT)
+figure('Name','WM vs GLIO (same CEST pools)');
+% M_z = Run_pulseq_cest_Simulation(seq_filename,fullfile(lib_path,'/sim-library/GM_3T_001_bmsim.yaml'));
+% Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset)
+% 
+sandbox_path='D:\root\LABLOG\FAU\11_CESTpulseq_standard\pulseq-cest-library\sandbox\001_realistic_APTw_3T';
+ M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/WM_3T_002_bmsim.yaml']);
+ [Z,w]= Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
+
+ M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/GLIO_3T_001_bmsim.yaml']);
+ [Z1,w]=Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
+ 
+  %% call standard sim  for WM and GLIO ( with shifted Lorentzian MT)
+figure('Name','WM vs GLIO (low GLIO_NOE)');
 % M_z = Run_pulseq_cest_Simulation(seq_filename,fullfile(lib_path,'/sim-library/GM_3T_001_bmsim.yaml'));
 % Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset)
 % 
@@ -20,9 +56,8 @@ sandbox_path='D:\root\LABLOG\FAU\11_CESTpulseq_standard\pulseq-cest-library\sand
  M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/GLIO_3T_002_bmsim.yaml']);
  [Z1,w]=Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
  
- 
- %% EMR?
- figure(3),
+  %% EMR?
+figure('Name','EMR in glioma');
   M_z = Run_pulseq_cest_Simulation(seq_filename,[sandbox_path '/GLIO_3T_002_bmsim.yaml']);
  [Z,w]= Plot_pulseq_cest_Simulation(M_z,offsets_ppm,m0_offset);
 
